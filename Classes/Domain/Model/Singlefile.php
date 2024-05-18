021cd5a542ff68e0430b74a17592810fb338ac01
Torben Hansen <derhansen@gmail.com>
2024-05-18 13:20:41 +0200

    [TASK] Added `imageDimensions` validator to Singlefile model

--- Classes/Domain/Model/Singlefile.php
+++ Classes/Domain/Model/Singlefile.php
@@ -22,7 +22,8 @@ class Singlefile extends AbstractEntity
             'required' => true,
             'maxFiles' => 1,
             'fileSize' => ['minimum' => '0K', 'maximum' => '2M'],
-            'allowedMimeTypes' => ['image/jpeg']
+            'allowedMimeTypes' => ['image/jpeg'],
+            'imageDimensions' => ['maxWidth' => 4096, 'maxHeight' => 4096]
         ],
         'uploadFolder' => '1:/user_upload/extbase_single_file/',
         'addRandomSuffix' => false,
