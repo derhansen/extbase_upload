8804b7909715fd6893a56ae0b6d68051653e69e2
Torben Hansen <derhansen@gmail.com>
2024-08-02 11:41:18 +0200

    [WIP][FEATURE] Provide validators for PSR-7 UploadedFile objects in Extbase
    todo:
    - Add commit message
    - Add feature RST
    
    Resolves: #104526
    Releases: main

--- Classes/Domain/Model/Dto/SinglefileDto.php
+++ Classes/Domain/Model/Dto/SinglefileDto.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Derhansen\ExtbaseUpload\Domain\Model\Dto;
 
 use TYPO3\CMS\Core\Http\UploadedFile;
-use TYPO3\CMS\Extbase\Annotation\Validate;
 use TYPO3\CMS\Extbase\Annotation\FileUpload;
+use TYPO3\CMS\Extbase\Annotation\Validate;
+use TYPO3\CMS\Extbase\Validation\Validator\FileSizeValidator;
+use TYPO3\CMS\Extbase\Validation\Validator\MimeTypeValidator;
 
 class SinglefileDto
 {
@@ -15,13 +17,18 @@ class SinglefileDto
     ])]
     protected string $title = '';
 
-    #[FileUpload([
-        'validation' => [
-            'required' => true,
-            'maxFiles' => 1,
-            'fileSize' => ['minimum' => '100K', 'maximum' => '2M'],
-            'allowedMimeTypes' => ['image/jpeg']
-        ],
+    #[Validate(['validator' => 'NotEmpty'])]
+    #[Validate([
+        'validator' => MimeTypeValidator::class,
+        'options' => [
+            'allowedMimeTypes' => ['image/jpeg'],
+        ]
+    ])]
+    #[Validate([
+        'validator' => FileSizeValidator::class,
+        'options' => [
+            'minimum' => '10M',
+        ]
     ])]
     protected ?UploadedFile $file = null;
 
